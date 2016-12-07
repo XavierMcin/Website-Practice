081f655a5a78de4c9b80dbe0059441ab7e35781d
Xavier McIntosh <xavier2794@gmail.com>
2016-12-07 18:05:38 -0500

    almost done with the header section just a few more tweaks

--- Website Practice 1/js/main.js
+++ Website Practice 1/js/main.js
@@ -3,3 +3,30 @@ $(function(){
 		$('.dropDown').toggleClass('appear');
 	});
 });
+
+
+$(function(){
+	$(window).scroll(function(event){
+		var target = $(this).scrollTop();
+
+		if (target > 3){
+			$('.headerWrapper').addClass('scrolling');
+			$('.topLeft > img').addClass('scrolling');
+			$('.topLeft h2').addClass('scrolling');
+			$('.random img').addClass('scrolling');
+			$('.random h3').addClass('scrolling');
+			$('.dropDown').addClass('scrolling');
+			$('header').addClass('scrolling');
+			$('.topRight nav span').addClass('scrolling');
+		} if (target == 0) {
+			$('.headerWrapper').removeClass('scrolling');
+			$('.topLeft > img').removeClass('scrolling');
+			$('.topLeft h2').removeClass('scrolling');
+			$('.random img').removeClass('scrolling');
+			$('.random h3').removeClass('scrolling');
+			$('.dropDown').removeClass('scrolling');
+			$('header').removeClass('scrolling');
+			$('.topRight nav span').removeClass('scrolling');
+		}
+	});
+});
